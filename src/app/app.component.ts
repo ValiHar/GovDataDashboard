3990ede77c12a370f6a5f7c6804bc61de18015be
Vali H <valentin.hartig@gmail.com>
2022-03-29 16:23:55 +0200

    Reduce complexity by removing layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<app-dashboard-layout></app-dashboard-layout>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'GovDataDashboard';
