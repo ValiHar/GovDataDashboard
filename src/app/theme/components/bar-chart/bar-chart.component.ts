208960a65911e6999240e9f52c9046643f0230dd
Vali H <valentin.hartig@gmail.com>
2022-03-29 18:45:28 +0200

    Remove old code

--- src/app/theme/components/bar-chart/bar-chart.component.ts
+++ src/app/theme/components/bar-chart/bar-chart.component.ts
@@ -87,38 +87,10 @@ export class BarChartComponent implements OnInit {
         {
           type: 'bar',
           showBackground: true,
-          // itemStyle: {
-          //   color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-          //     { offset: 0, color: '#83bff6' },
-          //     { offset: 0.5, color: '#188df0' },
-          //     { offset: 1, color: '#188df0' }
-          //   ])
-          // },
-          // emphasis: {
-          //   itemStyle: {
-          //     color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-          //       { offset: 0, color: '#2378f7' },
-          //       { offset: 0.7, color: '#2378f7' },
-          //       { offset: 1, color: '#83bff6' }
-          //     ])
-          //   }
-          // },
           data: []
         }
       ]
     };
-    
-    // Enable data zoom when user click bar.
-    const zoomSize = 6;
-    /* myChart.on('click', function (params) {
-      console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
-      myChart.dispatchAction({
-        type: 'dataZoom',
-        startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
-        endValue:
-          dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
-      });
-    }); */
   }
 
 }
