282b04153e4f9bec9939c2d7776ec4deb7803ca0
Vali H <valentin.hartig@gmail.com>
2022-03-29 16:21:21 +0200

    Add dataPoint model for charts

---
+++ src/app/core/models/dataPoint.ts
@@ -0,0 +1,9 @@
+export class DataPoint {
+    private value: number;
+    private name: string;
+
+    constructor(value: number, name: string) {
+        this.value = value;
+        this.name = name;
+    }
+}
