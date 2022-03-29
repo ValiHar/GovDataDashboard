35c9f1a440af894064830c4200c54098e2caed88
Vali H <valentin.hartig@gmail.com>
2022-03-29 18:48:00 +0200

    Test bar chart

--- src/app/theme/components/bar-chart/bar-chart.component.spec.ts
+++ src/app/theme/components/bar-chart/bar-chart.component.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { Department } from 'src/app/core/models/department';
+import { DataService } from 'src/app/core/services/data.service';
 
 import { BarChartComponent } from './bar-chart.component';
 
+class MockDataService {
+
+  public getDepartments(): Observable<Department[]> { return new Observable();}
+}
+
 describe('BarChartComponent', () => {
   let component: BarChartComponent;
+  let dataService: DataService;
   let fixture: ComponentFixture<BarChartComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BarChartComponent ]
+      declarations: [ BarChartComponent ],
+      providers: [
+        BarChartComponent,
+        { provide: DataService, useClass: MockDataService }
+      ]
     })
     .compileComponents();
+
+    component = TestBed.inject(BarChartComponent);
+    dataService = TestBed.inject(DataService);
   });
 
   beforeEach(() => {
@@ -21,5 +37,9 @@ describe('BarChartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the options property defined', () => {
+    expect(component.options).toBeDefined();
   });
 });
