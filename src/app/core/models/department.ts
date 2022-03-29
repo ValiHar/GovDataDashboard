b2bfb21341e52db60bb6342366c62290289a3de0
Vali H <valentin.hartig@gmail.com>
2022-03-29 18:44:38 +0200

    Change department model as getter and setter are not necessary

--- src/app/core/models/department.ts
+++ src/app/core/models/department.ts
@@ -1,35 +1,11 @@
 export class Department {
-    private _department: string;
-    private _description: string;
-    private _datasets: number;
+    public department: string;
+    public description: string;
+    public datasets: number;
 
     constructor(department: string, description: string, datasets: number) {
-        this._department = department;
-        this._description = description;
-        this._datasets = datasets;
-    }
-
-    get department(): string {
-        return this._department;
-    }
-
-    set department(department: string) {
-        this._department = department;
-    }
-
-    get description(): string {
-        return this._description;
-    }
-
-    set description(description: string) {
-        this._description = description;
-    }
-
-    get datasets(): number {
-        return this._datasets;
-    }
-
-    set datasets(datasets: number) {
-        this._datasets = datasets;
+        this.department = department;
+        this.description = description;
+        this.datasets = datasets;
     }
 }
