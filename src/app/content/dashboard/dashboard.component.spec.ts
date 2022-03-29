a8a206da9cd0c153e78eb864e66baea2fb6ee5fd
Vali H <valentin.hartig@gmail.com>
2022-03-29 18:47:50 +0200

    Test dashboard

--- src/app/content/dashboard/dashboard.component.spec.ts
+++ src/app/content/dashboard/dashboard.component.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { Department } from 'src/app/core/models/department';
+import { DataService } from 'src/app/core/services/data.service';
 
 import { DashboardComponent } from './dashboard.component';
 
+class MockDataService {
+
+  public getDepartments(): Observable<Department[]> { return new Observable();}
+}
+
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
+  let dataService: DataService;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        DashboardComponent,
+        { provide: DataService, useClass: MockDataService }
+      ]
     })
     .compileComponents();
+
+    component = TestBed.inject(DashboardComponent);
+    dataService = TestBed.inject(DataService);
   });
 
   beforeEach(() => {
