33c5b73cb19e5b302baf471b82c921eed611e4f2
Vali H <valentin.hartig@gmail.com>
2022-03-29 18:48:11 +0200

    Test pie chart

--- src/app/theme/components/pie-chart/pie-chart.component.spec.ts
+++ src/app/theme/components/pie-chart/pie-chart.component.spec.ts
@@ -1,16 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { Department } from 'src/app/core/models/department';
+import { DataService } from 'src/app/core/services/data.service';
 
 import { PieChartComponent } from './pie-chart.component';
 
+class MockDataService {
+
+  public getDepartments(): Observable<Department[]> { return new Observable(); }
+}
+
 describe('PieChartComponent', () => {
   let component: PieChartComponent;
+  let dataService: DataService;
   let fixture: ComponentFixture<PieChartComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PieChartComponent ]
+      declarations: [PieChartComponent],
+      providers: [
+        PieChartComponent,
+        { provide: DataService, useClass: MockDataService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
+
+    component = TestBed.inject(PieChartComponent);
+    dataService = TestBed.inject(DataService);
   });
 
   beforeEach(() => {
@@ -21,5 +37,9 @@ describe('PieChartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the options property defined', () => {
+    expect(component.options).toBeDefined();
   });
 });
